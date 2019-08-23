0f14a0f2f156109541c00c6579cfa93b2336904f
FJ98 <felix.solano@utec.edu.pe>
2019-08-23 11:54:07 -0500

    List of Conquests

--- main.cpp
+++ main.cpp
@@ -1,7 +1,24 @@
-#include <iostream>
+#include <bits/stdc++.h>
 
-using namespace std;
+int main() {
+    int numeroDeEntradasPermitidas = 0;
+    std::cin >> numeroDeEntradasPermitidas;
 
-int main(int argc, char *argv[]) {
+    std::map<std::string, int> contadorDeLasVecesEnQueApareceUnPais;
+
+    for (int i = 0; i < numeroDeEntradasPermitidas; ++i) {
+        std::string pais;
+        std::cin >> pais;
+
+        std::string primeraPalabraDeCadaLinea;
+        std::getline(std::cin, primeraPalabraDeCadaLinea);
+
+        ++contadorDeLasVecesEnQueApareceUnPais[pais];
+    }
+
+    for (auto && item: contadorDeLasVecesEnQueApareceUnPais) {
+        std::cout << item.first << " " << item.second << std::endl;
+    }
+
     return 0;
 }
